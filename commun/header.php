c7fc2cd2d9b727cc464f9f5e683bd3cc47efdbe3
Elysaph <elysaph@gmail.com>
2026-01-09 10:50:52 +0100

    correction pathing et texte

--- commun/header.php
+++ commun/header.php
@@ -15,21 +15,20 @@
 <body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="../index.php">Club</a>
+    <a class="navbar-brand" href=".\index.php">Club</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="..\index.php">Accueil</a>
+          <a class="nav-link active" aria-current="page" href=".\index.php">Accueil</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="../offres/offres.php">Offres</a>
+          <a class="nav-link" href="./offre/offres.php">Offres</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="espace-abonnee.php">Espace Abonné</a>
-      
         </li>
         </ul>
       </ul>
--- offre/offres.php
+++ offre/offres.php
@@ -8,36 +8,36 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </head>
 <body>
-  <?php include 'commun/header.php'; ?>
+  <?php include '../commun/header.php'; ?>
   <div class="container mt-4">
     <h1>Nos Offres</h1>
     <p>Découvrez nos offres spéciales pour les abonnés du club de sport.</p>
-    <!-- Contenu des offres -->
   </div>
   <section id="menu" class="py-5">
+        <!-- Contenu des offres -->
         <div class="container">
-          <h2 class="text-center mb-4">COURS DE JEUDI</h2>
+          <h2 class="text-center mb-4">Toutes nos offres</h2>
           <div class="row g-4"> <!--sert a aligner les cartes avec un espacement-->
              <div class="col-md-4">
               <div class="card h-100">
-                <img src="../img/logo_html.jpg" class="card-img-top" alt="logo HTML">
+                <img src="../img/logo_MMA.jpg" class="card-img-top" alt="logo MMA">
                 <div class="card-body d-flex flex-column">
-                  <h5 class="card-title">HTML</h5>
-                  <p class="card-text">Langage de structure : il sert à organiser le contenu d’une page web.</p>
+                  <h5 class="card-title">MMA</h5>
+                  <p class="card-text">Boxing etc.</p>
                   <div class="mt-auto d-flex justify-content-between align-items-center">
-                    <a href="../cours/cours_info/html.php"class="btn btn-outline-primary">voir le cours</a>
+                    <a href="../cours/cours_info/MMA.php"class="btn btn-outline-primary">Voir l'offre</a>
                   </div>
                 </div>
               </div>
             </div>
             <div class="col-md-4">
               <div class="card h-100">
-                <img src="../img/logo_css.jpg" class="card-img-top" alt="logo CSS">
+                <img src="../img/logo_Natation.jpg" class="card-img-top" alt="logo Natation">
                 <div class="card-body d-flex flex-column">
-                  <h5 class="card-title">CSS</h5>
-                  <p class="card-text">Langage de style : il contrôle l’apparence et la mise en forme du contenu HTML.</p>
+                  <h5 class="card-title">Natation</h5>
+                  <p class="card-text">Natation en piscine.</p>
                   <div class="mt-auto d-flex justify-content-between align-items-center">
-                   <a href="../cours/cours_info/css.php"class="btn btn-outline-primary">voir le cours</a>
+                   <a href="../cours/cours_info/Natation.php"class="btn btn-outline-primary">Voir l'offre</a>
                   </div>
                 </div>
               </div>
@@ -45,12 +45,12 @@
 
             <div class="col-md-4">
               <div class="card h-100">
-                <img src="../img/logo_sql.jpg" class="card-img-top" alt="logo SQL">
+                <img src="../img/logo_Yoga.jpg" class="card-img-top" alt="logo Yoga">
                 <div class="card-body d-flex flex-column">
-                  <h5 class="card-title">SQL</h5>
-                  <p class="card-text">Langage de requête : utilisé pour interagir avec les bases de données relationnelles (comme MySQL, PostgreSQL, Oracle).</p>
+                  <h5 class="card-title">Yoga</h5>
+                  <p class="card-text">Yoga quoi</p>
                   <div class="mt-auto d-flex justify-content-between align-items-center">
-                    <a href="../cours/cours_info/sql.php"class="btn btn-outline-primary">voir le cours</a>
+                    <a href="../cours/cours_info/Yoga.php"class="btn btn-outline-primary">Voir l'offre</a>
                   </div>
                 </div>
               </div>
@@ -60,6 +60,6 @@
         </div>
     </section>
 
-  <?php include 'commun/footer.php'; ?>
+  <?php include '../commun/footer.php'; ?>
 </body> 
 </html>
